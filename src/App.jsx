7c5f9dd473757c49664f4654e8d856d8e20bc8f1
Pattarapon363 <pattarapon.boonpa@gmail.com>
2024-09-27 15:00:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState,useReducer } from 'react';
+import AddTask from './AddTask.jsx';
+import TaskList from './TaskList.jsx';
+import tasksReducer from './tasksReducer.jsx';
+
+export default function TaskApp() {
+  // const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text: text,
+    });
+  }
+  
+  function handleChangeTask(task) {
+    dispatch({
+      type: 'changed',
+      task: task,
+    });
+  }
+  
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
+    });
+  }
+
+  function tasksReducer(tasks, action) {
+    switch (action.type) {
+      case 'added': {
+        return [
+          ...tasks,
+          {
+            id: action.id,
+            text: action.text,
+            done: false,
+          },
+        ];
+      }
+      case 'changed': {
+        return tasks.map((t) => {
+          if (t.id === action.task.id) {
+            return action.task;
+          } else {
+            return t;
+          }
+        });
+      }
+      case 'deleted': {
+        return tasks.filter((t) => t.id !== action.id);
+      }
+      default: {
+        throw Error('Unknown action: ' + action.type);
+      }
+    }
+  }
+
+  return (
+    <>
+      <h1 className='text-blue-400'>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Visit Kafka Museum', done: true},
+  {id: 1, text: 'Watch a puppet show', done: false},
+  {id: 2, text: 'Lennon Wall pic', done: false},
+];
